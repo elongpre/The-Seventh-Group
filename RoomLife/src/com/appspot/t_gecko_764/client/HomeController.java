567be1670dc963baa5a2f522237002e4cb0a45f4
elongpre <elongpre@gmail.com>
2014-11-25 14:54:45 -0600

    Commiting everyting

--- RoomLife/src/com/appspot/t_gecko_764/client/HomeController.java
+++ RoomLife/src/com/appspot/t_gecko_764/client/HomeController.java
@@ -7,7 +7,7 @@ import com.appspot.t_gecko_764.*;
 
 public class HomeController {
 
-	public List<Bill> getBills(){
+	public static List<Bill> getBills(){
 		ArrayList<Bill> bills = new ArrayList<Bill>();
 		bills.add(new Bill("ThisBill"));
 		bills.add(new Bill("ThatBill"));
