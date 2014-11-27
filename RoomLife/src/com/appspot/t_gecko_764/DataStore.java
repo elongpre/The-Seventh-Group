b46624f73757e8a89d90ef2dc93515610efc07e3
Guillermo <guillenunez28@yahoo.com>
2014-11-27 13:23:42 -0600

    Add function to datastore

--- RoomLife/src/com/appspot/t_gecko_764/DataStore.java
+++ RoomLife/src/com/appspot/t_gecko_764/DataStore.java
@@ -2,6 +2,8 @@ package com.appspot.t_gecko_764;
 
 import static com.googlecode.objectify.ObjectifyService.ofy;
 
+import java.util.ArrayList;
+
 
 public class DataStore {
 	private static DataStore uniqueInstance;
@@ -22,6 +24,16 @@ public class DataStore {
 	
 	public void savePerson(Person person){
 		ofy().save().entity(person).now();
-		
+	}
+	
+	public Group getGroup(Person person, String groupName){
+		ArrayList<Group> x= person.getGroups();
+		for(Group name: x ){
+			String findGroup= name.getName();
+			if (findGroup.equals(groupName)){
+				return name;
+			}
+		}
+		return null;
 	}
 }
