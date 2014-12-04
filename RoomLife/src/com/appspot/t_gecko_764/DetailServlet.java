3c5d803b1763130ed0a58eb360efb7584783006f
elongpre <elongpre@gmail.com>
2014-12-04 13:54:28 -0600

    fixed id error

--- RoomLife/src/com/appspot/t_gecko_764/DetailServlet.java
+++ RoomLife/src/com/appspot/t_gecko_764/DetailServlet.java
@@ -146,7 +146,8 @@ public class DetailServlet extends HttpServlet{
 				req.setAttribute("Request", request);
 				req.getRequestDispatcher("/WEB-INF/showRequest.jsp").forward(req, resp);
 				break;
-			case "building": 	Building building = datastore.getBuilding(id);
+			case "building": 	id = Long.parseLong(splitPath[length-1], 10);
+				Building building = datastore.getBuilding(id);
 				req.setAttribute("building", building);
 				req.getRequestDispatcher("/WEB-INF/showBuilding.jsp").forward(req, resp);
 
