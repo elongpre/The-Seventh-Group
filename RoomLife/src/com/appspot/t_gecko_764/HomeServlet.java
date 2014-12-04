c90b2b27fa76329325ce9b9828a2a56bd7a89de2
elongpre <elongpre@gmail.com>
2014-12-04 13:59:19 -0600

    Fixed merge comments in HomeServlet

--- RoomLife/src/com/appspot/t_gecko_764/HomeServlet.java
+++ RoomLife/src/com/appspot/t_gecko_764/HomeServlet.java
@@ -18,72 +18,36 @@ import com.google.appengine.api.users.UserServiceFactory;
 
 public class HomeServlet extends HttpServlet{
 
-		public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException{
-			UserService userService = UserServiceFactory.getUserService();
-		    User user = userService.getCurrentUser();
-		    
-		    if( user == null){
-		    	resp.sendRedirect("/LoginPage.jsp");
-		    }
-		    
-		    // get bills where person is owner
-			DataStore datastore = DataStore.getInstance();
-			Landlord landlord=datastore.getLandlord(user.getEmail());
-			Person person = datastore.getPerson("alice@example.com");
-<<<<<<< HEAD
-			
-			if(landlord!=null){
-				List<Long> buildingsKey=landlord.getBuildings();
-				ArrayList<Building> buildings=new ArrayList<Building>();
-				if(buildingsKey!=null){
-					for(Long buildingKey:buildingsKey){
-						
-						buildings.add(datastore.getBuilding(buildingKey));
-						
-					}
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException{
+		UserService userService = UserServiceFactory.getUserService();
+	    User user = userService.getCurrentUser();
+	    
+	    if( user == null){
+	    	resp.sendRedirect("/LoginPage.jsp");
+	    }
+	    
+	    // get bills where person is owner
+		DataStore datastore = DataStore.getInstance();
+		Landlord landlord=datastore.getLandlord(user.getEmail());
+		Person person = datastore.getPerson("alice@example.com");
+		
+		if(landlord!=null){
+			List<Long> buildingsKey=landlord.getBuildings();
+			ArrayList<Building> buildings=new ArrayList<Building>();
+			if(buildingsKey!=null){
+				for(Long buildingKey:buildingsKey){
+					
+					buildings.add(datastore.getBuilding(buildingKey));
+					
 				}
-				req.setAttribute("Buildings", buildings);
-				req.getRequestDispatcher("/WEB-INF/landlordHome.jsp").forward(req, resp);
-				
-			}else if (person!=null){
-=======
+			}
+			req.setAttribute("Buildings", buildings);
+			req.getRequestDispatcher("/WEB-INF/landlordHome.jsp").forward(req, resp);				
+		} else if (person!=null) {
 			String userEmail = person.getEmail();
->>>>>>> c759f699f89fcc80ce8e434040acf25854e8d49a
-			List<Bill> bills = datastore.getBills(person);
-			
-			
-			// get bills where person is not the owner
-			/*Bill bill;
-			List<Long> externalBills = person.getBills(); 
-			if(externalBills!=null){
-				for(Long id: externalBills){
-					bill=datastore.getBill(id);
-					bills.add(bill);
-				}
-			}*/
-			
-			
-			
-			
-			
+			List<Bill> bills = datastore.getBills(person);			
 			req.setAttribute("BillList", bills);
-			
-<<<<<<< HEAD
-			
-			
-			List<Debt> debts = datastore.getDebts(person);
-			Debt debt;
-			List<Long> externalDebts = person.getDebts(); 
-			
-			/*if(externalDebts!=null){
-				for(Long id: externalDebts){
-					debt=datastore.getDebt(id);
-					debts.add(debt);
-				}
-			}*/
-			
-			req.setAttribute("DebtList", debts);
-=======
+	
 			List<String> emails = datastore.getGroup(person.getGroup()).getMembers();
 			emails.remove(person.getEmail());
 			ArrayList<String> roommates = new ArrayList<String>();
@@ -116,23 +80,12 @@ public class HomeServlet extends HttpServlet{
 			
 			req.setAttribute("DebtNames", roommates);
 			req.setAttribute("DebtAmounts", charges);
->>>>>>> c759f699f89fcc80ce8e434040acf25854e8d49a
 			
-			List<MaintenanceRequest> requests = datastore.getMaintenanceRequests(person);
-			/*MaintenanceRequest mRequest;
-			List<Long> externalRequests = person.getRequests();
-			
-			if(externalRequests!=null){
-				for(Long id: externalRequests){
-					mRequest=datastore.getMaintenanceRequest(id);
-					requests.add(mRequest);
-			
-				}
-			}*/
-			
+			List<MaintenanceRequest> requests = datastore.getMaintenanceRequests(person);			
 			req.setAttribute("RequestList", requests);
+			
 			req.getRequestDispatcher("/WEB-INF/home.jsp").forward(req, resp);
-		}else{
+		} else {
 			resp.sendRedirect("/LoginPage.jsp");
 		}
 	}
